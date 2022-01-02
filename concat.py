bfb25ea33f5c584c747660f4240187c9bbc5eab1
V.K <family@fortress.by>
2022-01-03 01:57:18 +0300

    Function "concat"

--- concat.py
+++ concat.py
@@ -1,3 +1,27 @@
-def concat():
-    print(concat)
+def concat(*strings, reversed: bool):
+    lst = strings[0]
+    text = ''
+    if reversed == True:
+        lst.reverse()
+        for i in lst:
+            text += i + ' '
+    else:
+        for i in lst:
+            text += i + ' '
+    print(text)
 
+strings = []
+cnt = 1
+print("Проверка функции конкатенации строковых значений.")
+print("Вводите значения последовательно в любом количестве.")
+print("Для завершения ввода подстрок нажмите Enter.")
+while str:
+    str = input(f'Введите {cnt}-е строковое значение: ')
+    if str != '':
+        strings.append(str)
+        cnt += 1
+    else:
+        break
+r = bool(input('Если хотите выполнить "склеивание" с конца списка - введите True (по умолчанию значение False): '))
+
+concat(strings, reversed = r)
